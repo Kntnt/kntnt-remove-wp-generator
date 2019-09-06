4302d5db273b521c11ddcef126683e79ac360742
Thomas Barregren <thomas@kntnt.com>
2019-09-06 14:45:20 +0200

    Added test of ABSPATH.

--- kntnt-remove-wp-generator.php
+++ kntnt-remove-wp-generator.php
@@ -5,11 +5,13 @@
  * Plugin Name:       Kntnt Remove WP Generator
  * Plugin URI:        https://www.kntnt.com/
  * Description:       Stops Wordpress from announcing its version number to the world.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+defined( 'ABSPATH' ) || die;
 
 remove_action( 'wp_head', 'wp_generator' );
